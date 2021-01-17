1c2d0d03dd21bd4ebb259b76bd425389a7635d9c
Optimus666 <dajerweb@gmail.com>
2021-01-17 13:23:47 +0100

    implement OptimusTestPad.cs

---
+++ OptimusTestPad/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Menu_Generator.Thief;
+using Menu_Generator.Thief.Model;
+
+namespace OptimusTestPad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var filename = "Products.xml";
+            //var currentDirectory = Directory.GetCurrentDirectory();
+            //var purchaseOrderFilepath = Path.Combine($"{currentDirectory}/../../..", filename);
+
+
+            var sourceUrl = "https://egeszsegkonyha.hu/index.php/etlapunk?week=202103";
+            var targetFilePath = @"d:\work\FHCN\OptimusTestPad\Products.xml";
+
+            var xmlRooting = new XmlRooting(sourceUrl, targetFilePath);
+
+            Service.Processor(xmlRooting);
+        }
+    }
+}
